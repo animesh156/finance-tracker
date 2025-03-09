95752355782fbb6cfb52af95ae4a01eac4aacc9a
animesh <animeshrathore255@gmail.com>
2025-03-10 00:10:48 +0530

    initiliazed nextjs project

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+  <>
+  <h1 className="text-red-500">Hello cl</h1>
+  </>
+  );
+}
